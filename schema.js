a98908cc143135d22ebf4c7dccbeddcabde422b5
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-09-26 23:31:37 +0700

    Update muation data module

--- schema.js
+++ schema.js
@@ -1,37 +1,42 @@
 export const typeDefs = `#graphql
     type Game {
-        id: ID!,
-        title: String!,
-        platform: [String!]!,
+        id: ID!
+        title: String!
+        platform: [String!]!
         reviews: [Review!]
     }
     type Review {
-        id: ID!,
-        rating: Int!,
+        id: ID!
+        rating: Int!
         content: String!
-        game: Game!,
+        game: Game!
         author: Author!
     }
     type Author {
-        id: ID!,
-        name: String!,
-        verified: Boolean!,
+        id: ID!
+        name: String!
+        verified: Boolean!
         reviews: [Review!]
     }
     type Query {
-        reviews: [Review],
-        review(id: ID!): Review,
-        games: [Game],
-        game(id: ID!): Game,
-        authors: [Author],
+        reviews: [Review]
+        review(id: ID!): Review
+        games: [Game]
+        game(id: ID!): Game
+        authors: [Author]
         author(id: ID!): Author
     }
-    type Muation {
-        addGame(game: AddGameInput!): Game,
+    type Mutation {
+        addGame(game: AddGameInput!): Game
         deleteGame(id: ID!): [Game]
+        updateGame(id: ID!, edits: EditGameInput!): Game
     }
     input AddGameInput {
-        title: String!,
+        title: String!
         platform: [String!]!
+    }
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 `
