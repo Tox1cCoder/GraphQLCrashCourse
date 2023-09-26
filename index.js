a3cba22e87a243e8875fa6b89318cbdb68a3614a
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-09-26 22:42:50 +0700

    Add mutation data module

--- index.js
+++ index.js
@@ -46,6 +46,22 @@ const resolvers = {
         game(parent) {
             return db.games.filter((g) => g.id === parent.game_id)
         }
+    },
+    Muation: {
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random * 10000).toString()
+            }
+            db.games.push(game)
+
+            return game
+        },
+        deleteGame(_, args) {
+            db.games = db.games.filter((g) => g.id !== args.id)
+
+            return db.games
+        }
     }
 }
 
